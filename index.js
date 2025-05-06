e2c718ac47dca5bb53b66cc85989522892ed90c5
hadiyasameer <hadiyasameer2013@gmail.com>
2025-05-07 04:11:29 +0530

    updated index.js to solve cors issue

--- index.js
+++ index.js
@@ -13,22 +13,22 @@ connectDB();
 
 seedAdmin();
 
-const prodOrigins = [process.env.ORIGIN_1, process.env.ORIGIN_2].filter(Boolean)
-const devOrigin = ['http://localhost:5173']
-const allowedOrigins = process.env.NODE_ENV === 'production' ? prodOrigins : devOrigin
-app.use(
-    cors({
-        origin: (origin, callback) => {
-            if (!origin || allowedOrigins.includes(origin)) {
-                console.log(origin, allowedOrigins)
-                callback(null, true)
-            } else {
-                callback(new Error('Not allowed by CORS'))
-            }
-        },
-        methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
-        credentials: true
-    })
+
+const allowedOrigins = [
+    'https://car-rental-client-mauve.vercel.app',
+    'http://localhost:5173'
+]
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"]
+})
 )
 app.use(express.json())
 
