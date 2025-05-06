58c7b34b47ea5f232aa461a56324ac1a70520986
hadiyasameer <hadiyasameer2013@gmail.com>
2025-05-07 02:25:33 +0530

    updated index.js to resolve cors issue

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express"
 import { connectDB } from "./config/db.js"
 import cors from "cors"
-const app=express()
-import cookieParser from'cookie-parser'
+const app = express()
+import cookieParser from 'cookie-parser'
 import { apiRouter } from "./routes/index.js"
 import dotenv from "dotenv";
 import seedAdmin from "./utils/seedAdmin.js"
@@ -13,27 +13,36 @@ connectDB();
 
 seedAdmin();
 
-app.use(express.json())
-
+const prodOrigins = [process.env.ORIGIN_1, process.env.ORIGIN_2].filter(Boolean)
+const devOrigin = ['http://localhost:5173']
+const allowedOrigins = process.env.NODE_ENV === 'production' ? prodOrigins : devOrigin
 app.use(
     cors({
-        origin:'http://localhost:5173',
-        methods:["GET","PUT","POST","DELETE","OPTIONS"],
-        credentials:true
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                console.log(origin, allowedOrigins)
+                callback(null, true)
+            } else {
+                callback(new Error('Not allowed by CORS'))
+            }
+        },
+        methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+        credentials: true
     })
 )
+app.use(express.json())
 
 app.use(cookieParser());
 
-const port=process.env.port||3000;
+const port = process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("hi")
 })
 
-app.use("/api",apiRouter)
+app.use("/api", apiRouter)
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
 
