2bb838e9aa08833e0b1939b12573648176036fb8
hadiyasameer <hadiyasameer2013@gmail.com>
2025-05-10 02:12:32 +0530

    added a new dealercontroller to get all dealers

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,10 +1,12 @@
 import e from "express"
 import { adminLogin, adminLogout } from "../controllers/adminControllers.js";
 import { adminAuth } from "../middlewares/adminAuth.js";
+import { getAllDealers } from "../controllers/dealerControllers.js";
 
 const router = e.Router();
 
 router.post("/login",adminLogin)
 router.post("/logout", adminAuth, adminLogout);
+router.get("/admin/dealers", adminAuth,getAllDealers)
 
 export { router as adminRouter }
